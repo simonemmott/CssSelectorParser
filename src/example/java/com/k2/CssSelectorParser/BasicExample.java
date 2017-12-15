a0e8414d735202da4c9055d5c0db92ce3dcd841c
Simon Emmott <simon.emmott@yahoo.co.uk>
2017-12-15 20:06:12 +0000

    Added sample output to BasicExample

--- src/example/java/com/k2/CssSelectorParser/BasicExample.java
+++ src/example/java/com/k2/CssSelectorParser/BasicExample.java
@@ -3,6 +3,22 @@ package com.k2.CssSelectorParser;
 import java.util.List;
 
 public class BasicExample {
+	
+	// This example produces the following output
+	// 
+	// The CSS selector: .myClass > img[src$=.jpg], ol#thisId + li[data-myData='some data value'] was parsed to generate
+	// 
+	// HAS_CLASS myClass
+	// IS_PARENT of
+	// TAG_EQUALS img and
+	// src ATTRIBUTE_ENDS_WITH .jpg
+	// Or
+	// TAG_EQUALS ol and
+	// ID_EQUALS thisId
+	// PREVIOUS_SIBLING of
+	// TAG_EQUALS li
+	// data-myData ATTRUBUTE_EQUALS some data value
+
 
 	public static void main(String[] args) {
 		
@@ -13,16 +29,32 @@ public class BasicExample {
 		System.out.println();
 		System.out.println("The CSS selector: "+cssSelector+" was parsed to generate");
 		System.out.println();		
-		System.out.println(filters.get(0).previousFilter.elementFilterRules.get(0).type + " " + filters.get(0).previousFilter.elementFilterRules.get(0).check);
+		System.out.println(
+				filters.get(0).previousFilter.elementFilterRules.get(0).type + " " + 
+				filters.get(0).previousFilter.elementFilterRules.get(0).check);
 		System.out.println(filters.get(0).rule + " of");
-		System.out.println(filters.get(0).elementFilterRules.get(0).type + " " + filters.get(0).elementFilterRules.get(0).check + " and");
-		System.out.println(filters.get(0).elementFilterRules.get(1).attribute + " " + filters.get(0).elementFilterRules.get(1).type + " " + filters.get(0).elementFilterRules.get(1).check);
+		System.out.println(
+				filters.get(0).elementFilterRules.get(0).type + " " + 
+				filters.get(0).elementFilterRules.get(0).check + " and");
+		System.out.println(
+				filters.get(0).elementFilterRules.get(1).attribute + " " + 
+				filters.get(0).elementFilterRules.get(1).type + " " + 
+				filters.get(0).elementFilterRules.get(1).check);
 		System.out.println("Or");
-		System.out.println(filters.get(1).previousFilter.elementFilterRules.get(0).type + " " + filters.get(1).previousFilter.elementFilterRules.get(0).check + " and");
-		System.out.println(filters.get(1).previousFilter.elementFilterRules.get(1).type + " " + filters.get(1).previousFilter.elementFilterRules.get(1).check);
+		System.out.println(
+				filters.get(1).previousFilter.elementFilterRules.get(0).type + " " + 
+				filters.get(1).previousFilter.elementFilterRules.get(0).check + " and");
+		System.out.println(
+				filters.get(1).previousFilter.elementFilterRules.get(1).type + " " + 
+				filters.get(1).previousFilter.elementFilterRules.get(1).check);
 		System.out.println(filters.get(1).rule + " of");
-		System.out.println(filters.get(1).elementFilterRules.get(0).type + " " + filters.get(1).elementFilterRules.get(0).check);
-		System.out.println(filters.get(1).elementFilterRules.get(1).attribute + " " + filters.get(1).elementFilterRules.get(1).type + " " + filters.get(1).elementFilterRules.get(1).check);
+		System.out.println(
+				filters.get(1).elementFilterRules.get(0).type + " " + 
+				filters.get(1).elementFilterRules.get(0).check);
+		System.out.println(
+				filters.get(1).elementFilterRules.get(1).attribute + " " + 
+				filters.get(1).elementFilterRules.get(1).type + " " + 
+				filters.get(1).elementFilterRules.get(1).check);
 		
 	}
 
